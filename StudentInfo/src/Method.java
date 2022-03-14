047ba353382fb36d313af867e3434347c3921c3c
krisxia0506 <xiajiayi0506@gmail.com>
2022-03-14 10:50:53 +0800

    JAVA中方法的使用1

--- StudentInfo/src/Method.java
+++ StudentInfo/src/Method.java
@@ -5,32 +5,39 @@ import java.util.Scanner;
 
 
 public class Method {
-        public static void main(String[] args) {
-            String result="PASS";
-            Scanner sc = new Scanner(System.in);// sc自定义名称，仅创建一次对象
+    public static void main(String[] args) {
+        String result="PASS";
+        Scanner sc = new Scanner(System.in);// sc自定义名称，仅创建一次对象
 
-            System.out.println("输入学生姓名");
-            String name=sc.nextLine();//多个方法
-            System.out.println("输入学生年龄");
-            int age=sc.nextInt();
-            System.out.println("输入学生成绩");
+        System.out.println("输入学生姓名");
+        String name=sc.nextLine();//多个方法
+        System.out.println("输入学生年龄");
+        int age=sc.nextInt();
+        System.out.println("输入学生成绩");
 
-            double grade = sc.nextDouble();
-            if (grade<60){
-                result="No PASS";
-            } else if(grade>=60&&grade<75){
-                result="合格";
-            } else if(grade>=75&&grade<90){
-                result="良好";
-            } else if(grade>=90){
-                result="优秀";
-            }
-            System.out.println("姓名："+name);
-            System.out.println("年龄："+age);
-            System.out.println("成绩："+grade);
-            System.out.println(result);
+        double grade = sc.nextDouble();
+        if (grade<60){
+            result="No PASS";
+        } else if(grade>=60&&grade<75){
+            result="合格";
+        } else if(grade>=75&&grade<90){
+            result="良好";
+        } else if(grade>=90){
+            result="优秀";
         }
-
+        System.out.println("姓名："+name);
+        System.out.println("年龄："+age);
+        System.out.println("成绩："+grade);
+        System.out.println(result);
+    }
+    //void表示方法没有返回值
+    public static void shuchu (String name,int age,double grade,String result){
+        System.out.println("姓名："+name);
+        System.out.println("年龄："+age);
+        System.out.println("成绩："+grade);
+        System.out.println(result);
+    }
+    //1111111111111111111111
     public static String judge (double grade) {
         String result="";
         if (grade<60){
