dfcb8d6e77234d21dd7ea7881a3fccfe1edd970c
krisxia0506 <xiajiayi0506@gmail.com>
2022-03-21 19:59:12 +0800

    构造方法的实操

--- HomeWork7/src/TestMobilePhone.java
+++ HomeWork7/src/TestMobilePhone.java
@@ -1,4 +1,4 @@
-public class Test {
+public class TestMobilePhone {
     public static void main(String[] args) {
         MobilePhone p1 = new MobilePhone("HUAWEIMate30","16945678999",3380);
         MobilePhone p2 = new MobilePhone("HUAWEIMateP50");
